3d9f8404f279db30270e2dd67350e7f790cfcd5f
lycioandreatto <lycioandreatto13@gmail.com>
2026-01-28 14:13:39 -0300

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ if not cookies.ready():
     st.stop()
 
 # --- CONFIGURAÇÃO DA PÁGINA ---
-st.set_page_config(page_title="Maratá - SCA, page_icon="📅", layout="wide")
+st.set_page_config(page_title="Maratá - SCA", page_icon="📅", layout="wide")
 
 # --- ESTILIZAÇÃO DOS CARDS E PERFIL ---
 st.markdown("""
