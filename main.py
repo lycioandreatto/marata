56b2778d3e72e5e287734e7682ce7d8764e73d21
lycioandreatto <lycioandreatto13@gmail.com>
2026-02-02 21:59:44 -0300

    Update main.py

--- main.py
+++ main.py
@@ -563,7 +563,7 @@ section[data-testid="stSidebar"] div[role="radiogroup"] > label:has(input[type="
   right: -75px;      /* joga pra fora do sidebar */
   width: 90px;       /* “ponte” até a tela */
   height: 100%;
-  background: #ffffff;   /* usa a MESMA cor/gradiente do item selecionado */
+  background: linear-gradient(90deg, rgba(0, 78, 173, 1) 0%, #ffffff 75%);   /* usa a MESMA cor/gradiente do item selecionado */
   border-top-right-radius: 16px;
   border-bottom-right-radius: 16px;
 
