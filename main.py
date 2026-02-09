33eec19c1759ceeaf3d3fcdc97b8e8d7e5d6f0b3
lycioandreatto <lycioandreatto13@gmail.com>
2026-02-09 15:35:34 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-st.markdown("### 🧪 DEBUG DRIVE")
-st.write("Folder ID (secrets):", repr(st.secrets["drive"]["folder_id"]))
-st.write("Service Account:", st.secrets["gcp"]["client_email"])
-
-
 import streamlit as st
 from geoloc import capturar_coordenadas
 import numpy as np
@@ -17,6 +12,12 @@ import time
 import os
 import math
 import streamlit as st
+
+st.markdown("### 🧪 DEBUG DRIVE")
+st.write("Folder ID (secrets):", repr(st.secrets["drive"]["folder_id"]))
+st.write("Service Account:", st.secrets["gcp"]["client_email"])
+
+
 
 import io
 import uuid
